87c97e757c7bc15c2f8d75c80fbbd3a2ccd519ad
MattieWasTaken <matthewcastles@hotmail.com>
2022-05-04 18:45:05 +1000

    Working Comment Feature Added

--- plugin.js
+++ plugin.js
@@ -1,11 +1,11 @@
-var post= document.getElementById("addComment")
+var post= document.getElementById("post");
+post.addEventListener("click", addComment);
 
-comment = function(){
-    var commentBoxValue = document.getElementById("commentBox").value;
+function addComment(){
+    var commentBoxValue= document.getElementById("comment-box").value;
     var li = document.createElement("li");
     var text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
+    document.getElementById("comment-box").value='';
 }
-
-post.addEventListener("click", comment);
