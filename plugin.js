9cfcd2cdd5bd34621a1e917075faff50f621bee4
Kritsanakiw <101153850+Kritsanakiw@users.noreply.github.com>
2022-05-05 14:17:20 +1000

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -1,9 +1,11 @@
-var post= document.getElementById("submit");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("commentBox").value;
+var post= document.getElementById("post");
+post.addEventListener("click", addComment);
+
+function addComment(){
+    var commentBoxValue= document.getElementById("comment-box").value;
     var li = document.createElement("li");
     var text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
- 
-});
+    document.getElementById("comment-box").value='';
+}
