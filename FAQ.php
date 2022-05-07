a194c7ea719f1bc0dfb4ca8902018c8e6a229253
Kritsanakiw <101153850+Kritsanakiw@users.noreply.github.com>
2022-05-07 21:00:24 +1000

    FAQ page, need to do links

---
+++ FAQ.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Potato Forum </title>
+<link rel="stylesheet" ref="stylesheet" href="style.css"/>
+<header>
+
+<div>
+	<img src="https://i.ibb.co/894Xm19/278471543-727015038329277-2731362503377802712-n.png" alt="Site Logo" border="0" style="width:75px; height:75px;" img align="left">
+    
+    <div class="row">
+        
+        <div class="column left">
+            <p class="mainheading" > OnlineTopia.com </p>
+
+            <form action="/action_page.php">
+                <input type="text" name="q" id="" placeholder="Search...">
+                <button type="submit">Submit</button> 
+            </form>
+        </div>
+        
+        <div class="column middle">
+            <a href="createpost.php"><button>Contact Us</button></a>
+            <a href="createpost.php"><button>F.A.Q</button></a>
+            <a href="createpost.php"><button>Submit Post</button></a>
+            <br>  
+        </div>
+            
+        <div class="column right">
+            <button class="smallbutton" href="#CreateAcc"> Create Account </button>
+            <button class="smallbutton" href="#LOGINPAGE"> Log In </button>
+        </div>
+        
+    </div>
+
+</div>
+    
+</header> 
+
+<body>
+	<br><br><br><br>
+    <div class="container">
+        <div class="subforum">
+			<div class="subforum-title">
+                <h1>We are here to help.</h1>
+            </div>
+
+            <div class="subforum-row">
+                <div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>Report problem</p>
+                </div>
+				
+				 <div class="subforum-description subforum-column">
+					<textarea type="text" name="content" id="content" placeholder="Tell us what happened. . ."></textarea>
+					<button type="submit" name="submit">Submit</button>
+                </div>
+            </div>
+			
+			<div class="subforum-title">
+            
+				<form action="/action_page.php">
+                <input type="text" name="q" id="" placeholder="Search keywords...">
+                <button type="submit">Dig the words!</button> 
+            </form>
+            </div>
+			
+			<div class="subforum-title">
+                <h1>Here is what we were asked lately. . .</h1>
+            </div>
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>How to create an account?</p>
+                </div>
+				
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>Cannot find a certain page? Here is how to!</p>
+                </div>
+				
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>Found something not right? Here is how to Report an unappropriate post!</p>
+                </div>
+			
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>What can we do?</p>
+                </div>
+				
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <p>Want to learn how to learn to do these stuffs? You better not!</p>
+                </div>
+		</div>
+     
+
+
+    </div>
+
+	<footer>
+	<div class="footerarea">
+		<f class="active" href="#Contact Us" style = "border:red; border-width:2px; border-style:solid;">Contact US</f>
+        <f href="#F.A.Q." style = "border:red; border-width:2px; border-style:solid;">F.A.Q.</f>
+        <f href="#Support" style = "border:red; border-width:2px; border-style:solid;">Support</f>
+		<f href="#About Us" style = "border:red; border-width:2px; border-style:solid;">About Us</f>
+        <f href="#Content Policy" style = "border:red; border-width:2px; border-style:solid;">Content Policy</f>
+	</div>
+	
+	
+	</footer>
+
+    <script src="main.js>"> </script>
+
+    </body>
+
+
+
+
+
+
+</html>
