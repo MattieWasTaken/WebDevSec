2a3a62b272fe4fca043020a7c9d5a4215f8fb200
Kritsanakiw <101153850+Kritsanakiw@users.noreply.github.com>
2022-05-08 14:03:25 +1000

    content policy

---
+++ contentpolicy.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Potato Forum </title>
+<link rel="stylesheet" ref="stylesheet" href="style.css"/>
+<header>
+
+<div>
+	<img src="https://i.ibb.co/894Xm19/278471543-727015038329277-2731362503377802712-n.png" alt="Site Logo" border="0" style="width:75px; height:75px;" img align="left">
+    
+    <div class="row">
+        
+        <div class="column left">
+        <a href="index.php"> <p class="mainheading" > OnlineTopia.com </p></a>
+
+            <form action="/action_page.php">
+                <input type="text" name="q" id="" placeholder="Search...">
+                <button type="submit">Submit</button> 
+            </form>
+        </div>
+        
+        <div class="column middle">
+            <a href="aboutus.php"><button>Contact Us</button></a>
+            <a href="index.php"><button>Home</button></a>
+            <a href="createpost.php"><button>Submit Post</button></a>
+            <br>  
+        </div>
+            
+        <div class="column right">
+            <button class="smallbutton" href="#CreateAcc"> Create Account </button>
+            <button class="smallbutton" href="#LOGINPAGE"> Log In </button>
+        </div>
+        
+    </div>
+
+</div>
+    
+</header> 
+
+<body>
+	<br><br><br><br>
+    <div class="container">
+        <div class="subforum">
+			<div class="subforum-title">
+                <h1>OnlineTopia Content Policy.</h1>
+            </div>
+
+			<div class="subforum-title">
+                <h2>Here are rules that you must comply with. . .</h2>
+            </div>
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+					<h3>Rule 1</h3>
+                    <p>OnlineTopia is a place where we are free to create any content that entertains people and help them learn something new. However, unapropriate content will not be tolerated such racist, sexual harass, attacted message etc.</p>
+                </div>
+				
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <h3>Rule 2</h3>
+                    <p>Respect the privacy of the others. Revealing other personal or sensitive information is not allowed.</p>
+                </div>
+				
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <h3>Rule 3</h3>
+                    <p>We do not encourage to use real personal information here, as this should be a free space, we do not want anyone reveal their personal information.</p>
+                </div>
+			
+				
+				<div class="subforum-icon subform-column center">
+                    <i class="fa fa-car"></i>
+                    <h3>Rule 4</h3>
+                    <p>Ensure to report any unapproprite or misconduct post to the developers.</p>
+					</div>
+			
+		</div>
+     
+
+
+    </div>
+
+	<footer>
+	<div class="footerarea">
+		<a href="ContactUs.php"> <f style = "border:red; border-width:2px; border-style:solid;">Contact US</f></a>
+        <a href="FAQ.php"><f  style = "border:red; border-width:2px; border-style:solid;">F.A.Q.</f></a>
+        <a href="support.php"><f style = "border:red; border-width:2px; border-style:solid;">Support</f></a>
+		<a href="aboutus.php"> <f style = "border:red; border-width:2px; border-style:solid;">About Us</f></a>
+        <a href="contentpolicy.php"><f href="#Content Policy" style = "border:red; border-width:2px; border-style:solid;">Content Policy</f></a>
+	</div>
+	
+	
+	</footer>
+
+    <script src="main.js>"> </script>
+
+    </body>
+
+
+
+
+
+
+</html>
