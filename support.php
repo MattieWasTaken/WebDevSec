896210f77b5b730b18c2cd3c5c666bc14f2c2575
MattieWasTaken <matthewcastles@hotmail.com>
2022-05-30 12:53:32 +1000

    implementing footer.php

--- support.php
+++ support.php
@@ -109,21 +109,7 @@
 
 
     </div>
-
-	<footer>
-	<div class="footerarea">
-		<a href="ContactUs.php"> <f style = "border:red; border-width:2px; border-style:solid;">Contact US</f></a>
-        <a href="FAQ.php"><f  style = "border:red; border-width:2px; border-style:solid;">F.A.Q.</f></a>
-        <a href="support.php"><f style = "border:red; border-width:2px; border-style:solid;">Support</f></a>
-		<a href="aboutus.php"> <f style = "border:red; border-width:2px; border-style:solid;">About Us</f></a>
-        <a href="contentpolicy.php"><f href="#Content Policy" style = "border:red; border-width:2px; border-style:solid;">Content Policy</f></a>
-        <div class="copyright">
-        <small>&copy; Copyright 2018, IMD Internet Solutions Inc</small>
-        </div>
-    </div>
-	
-	
-	</footer>
+	<?php include_once 'footer.php';?>
 
     <script src="main.js>"> </script>
 
