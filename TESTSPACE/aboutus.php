b73e31a63659811a9b56986129d32cc6de2ad09b
Kritsanakiw <101153850+Kritsanakiw@users.noreply.github.com>
2022-06-02 15:34:06 +1000

    Create aboutus.php

---
+++ TESTSPACE/aboutus.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>AboutUs Forum</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+
+<?php 
+include_once('header.php');
+include_once('databaseConnection.php');
+?>
+  
+<body>
+<div class="container-fluid p-1 bg-dark">
+<div class="container-fluid">
+    <div class="row p-3 mb-2 mt-2 bg-secondary text-white rounded">
+        <h3 class="text-left">About (WEBNAME) Developers</h3>
+    </div>
+</div>
+<?php 
+
+    echo "  
+    <div class='container-fluid'>
+    <div class='row'>
+    <div class='col-sm-1 p-3 mb-2 bg-secondary text-white rounded ml-2 mr-1'>
+    
+    <span><a class='text-light' href='personal link'>Code Developer</a></span>
+    </form>
+    </div>
+    <div class='col-lg p-3 mb-2 bg-secondary text-white rounded ml-1 mr-1'>
+    <a class='text-white' href='personal link'><h5 class='text-left'>Matthew Castles</h3></a>
+   <small class='overflow-hidden'>Details</small>
+   </form>
+    </div>
+    <div class='col-sm-1 p-3 mb-2 bg-secondary text-white rounded ml-1 mr-2'>
+    </form>
+    </div>      
+    </div>
+</div>"
+?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+
+  <?php include_once('footer.php')?>
+</html>
