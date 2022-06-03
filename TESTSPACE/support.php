63924c51edb98145cebaaaa4aad841fe5bf18217
Kritsanakiw <101153850+Kritsanakiw@users.noreply.github.com>
2022-06-04 07:54:15 +1000

    sup

--- TESTSPACE/support.php
+++ TESTSPACE/support.php
@@ -41,50 +41,49 @@ include_once('databaseConnection.php');
 		
         <div class="container-fluid">
             <div class="row">    
-                <div class='col-lg p- mb-2 bg-secondary rounded ml-2 mr-1 '>
-                    <h2>Gaming?</h2>
-                </div>
-                <div class='col-lg p- mb-2 bg-secondary rounded ml-2 mr-1 '>
-                    <h3>Some Gaming Suggestions For People Looking To Try New Games.</h3>
+                <div class='col-sm-5 p- mb-2 bg-secondary rounded ml-2 mr-1 '>
+                    <h2><a href=" ">Gaming?</a></h2>
                 </div>
             </div>
 
-                <div>  
-                    <p>MMORPG: Runescape</p>
+            <p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    Some Gaming Suggestions For People Looking To Try New Games.
+                </button>
+            </p>
+          
+            <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                <div class="card card-body col-sm-5 p- mb-2 bg-secondary rounded ml-2 mr-1">
+                        <a href=" ">MMORPG: Runescape</a>
+                        <a href=" ">RPG: Elden Ring</a>
+                        <a href=" ">Turn-based: Age Of Mythology</a>
+                        <a href=" ">Indie: Life is Strange</a>
+                </div>        
+            </div>    
+        </div>  
+
+        <div class="container-fluid">
+            <div class="row">  
+                <div class='col-sm-5 p- mb-2 bg-secondary rounded ml-2 mr-1 '>
+                        <h2><a href=" ">Have too much time too use?</a></h2>    
                 </div>
-            
-                <div>
-                    
-                    <p>RPG: Elden Ring</p>
-                </div>
-            
-                <div> 
-                    <p>Turn-based: Age Of Mythology</p>
-                </div>
-            
-                <div>                   
-                    <p>Indie: Life is Strange</p>
-                </div>
-        </div>    
-        
-                <h2>Have too much time too use?</h2>
-				<h4>Read these then!?</h4>
-            
-                <div>
-                    
-                    <p>Check out the home page for the most recent posts by clicking on OnlineTopia.com</p>
-                </div>
-            
-                <div>
-                   
-                    <p>Watch Youtube videos to learn how to make a website like this one.</p>
-                </div>
-            
-                <div>
-                   
-                    <p>Wanna be cool like Inbus? He's not cool either, dont worry.</p>
-                </div>
-           
+            </div>
+
+            <p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">        
+                        Read these then!?
+                </button>
+            </p>
+
+            <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                <div class="card card-body col-sm-5 p- mb-2 bg-secondary rounded ml-2 mr-1 ">
+                        <a href=" ">Check out the home page for the most recent posts by clicking on OnlineTopia.com</a>
+                        <a href=" ">Watch Youtube videos to learn how to make a website like this one.</a>
+                        <a href=" ">Wanna be cool like Inbus? He's not cool either, dont worry.</a>   
+                </div>        
+            </div> 
+        </div>
+
     </div>
 
  
@@ -94,6 +93,7 @@ include_once('databaseConnection.php');
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
   </body>
 
   <?php include_once('footer.php')?>
