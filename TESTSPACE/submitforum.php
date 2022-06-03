78f40994a4a200c0d34dad73e3caa1747c6d8f5b
MattieWasTaken <matthewcastles@hotmail.com>
2022-06-04 08:48:46 +1000

    Update submitforum.php

--- TESTSPACE/submitforum.php
+++ TESTSPACE/submitforum.php
@@ -11,16 +11,16 @@ echo "$topicID <br> $topicArea <br> $topicName";
 
 $stmt = $conn->prepare("INSERT INTO topic VALUES (?,?,?)");
 $stmt ->bind_param("sss",$topicID, $topicArea, $topicName);
-
+/*
 if($topicArea=="" || $topicName==""){
-    header("Location: createpost.php?post=failed");
+    header("Location: createtopic.php?post=failed");
 }else if(strlen($topicArea)>100){
-    header("Location: createpost.php?post=failedtooLong");
+    header("Location: createtopic.php?post=failedtooLong");
 }else if($user_id==""){
-    header("Location: createpost.php?post=failedNoLogin");
+    header("Location: createtopic.php?post=failedNoLogin");
 }else{
     $stmt->execute();
     header("Location: index.php?post=success");
 }
-
+*/
 ?>
