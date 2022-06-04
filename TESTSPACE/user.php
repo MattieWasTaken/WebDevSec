06c13d0e55107373f276e378e84c30e3ee6fed0f
MattieWasTaken <matthewcastles@hotmail.com>
2022-06-04 10:49:06 +1000

    Update user.php

--- TESTSPACE/user.php
+++ TESTSPACE/user.php
@@ -74,37 +74,41 @@ $userID = $_GET['user_id'];
 
     </div>
     <?php 
-    $query = "SELECT * FROM `forum_posts` WHERE user_id='$username';";
-    $result = mysqli_query($conn, $query);
-    $resultCheck = mysqli_num_rows($result);
-    $counter=0;
-    $postCounter=1;
-    while($rows[]=mysqli_fetch_array($result)){
-        $title= $rows[$counter]['title'];
-        $topicID = $rows[$counter]['topic_id'];
-        $userID = $rows[$counter]['user_id'];
-        $subtopic = $rows[$counter]['subtopic'];
-        $date = $rows[$counter]['date_submitted'];
-        echo "   
-        <div class='container-fluid'>
-        <div class='row'>
-        <div class='col-sm-1 p-3 mb-2 bg-secondary text-center text-white rounded ml-2 mr-1'>
-        <span>$postCounter.</span>
-        </div>
-        <div class='col-lg p-3 mb-2 bg-secondary text-white rounded ml-1 mr-1'>
-        <form method='GET'>
-        <input type='hidden' name='$topicID' $topicID> 
-        <a class='text-white' href='Post.php?topic_id=$topicID&subtopic=$subtopic'><h5 class='text-left'>$title</h3></a>
-        </form>
-        </div>
-        <div class='col-sm-1 p-3 mb-2 bg-secondary text-white rounded ml-1 mr-2'>
-        <form method='GET' name'$userID' $userID>
-        <p class='text-left'>Date: $date</p>
-        <a class='text-white' href='user.php?user_id=$userID&display=posts'> <small>By: $userID</small></a>
-        </form>
-        </div>      
-        </div>
-    </div>";
+
+    if($_GET['display']=="posts"){
+        $query = "SELECT * FROM `forum_posts` WHERE user_id='$username';";
+        $result = mysqli_query($conn, $query);
+        $resultCheck = mysqli_num_rows($result);
+        $counter=0;
+        $postCounter=1;
+        while($rows[]=mysqli_fetch_array($result)){
+            $title= $rows[$counter]['title'];
+            $topicID = $rows[$counter]['topic_id'];
+            $userID = $rows[$counter]['user_id'];
+            $subtopic = $rows[$counter]['subtopic'];
+            $date = $rows[$counter]['date_submitted'];
+            echo "   
+            <div class='container-fluid'>
+            <div class='row'>
+            <div class='col-sm-1 p-3 mb-2 bg-secondary text-center text-white rounded ml-2 mr-1'>
+            <span>$postCounter.</span>
+            </div>
+            <div class='col-lg p-3 mb-2 bg-secondary text-white rounded ml-1 mr-1'>
+            <form method='GET'>
+            <input type='hidden' name='$topicID' $topicID> 
+            <a class='text-white' href='Post.php?topic_id=$topicID&subtopic=$subtopic'><h5 class='text-left'>$title</h3></a>
+            </form>
+            </div>
+            <div class='col-sm-1 p-3 mb-2 bg-secondary text-white rounded ml-1 mr-2'>
+            <form method='GET' name'$userID' $userID>
+            <p class='text-left'>Date: $date</p>
+            <a class='text-white' href='user.php?user_id=$userID&display=posts'> <small>By: $userID</small></a>
+            </form>
+            </div>      
+            </div>
+        </div>";
+    }
+
     }
 
     ?>
