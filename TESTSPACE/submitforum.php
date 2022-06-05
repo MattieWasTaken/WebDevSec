4f8ad8e6026ef928b40ae4e024ba237b8d15fc68
MattieWasTaken <matthewcastles@hotmail.com>
2022-06-05 15:08:58 +1000

    fixedCreate Topic

--- TESTSPACE/submitforum.php
+++ TESTSPACE/submitforum.php
@@ -3,24 +3,24 @@
 session_start();
 include_once 'databaseConnection.php';
 
-$topicID = "4";
-$topicArea = $_POST['title'];
-$topicName = $_POST['content'];
+$topicName = $_POST['title'];
+$topicDescription = $_POST['content'];
+$user_id = $_SESSION['username'];
 
-echo "$topicID <br> $topicArea <br> $topicName";
+echo "$topicName <br> $topicDescription";
 
-$stmt = $conn->prepare("INSERT INTO topic VALUES (?,?,?)");
-$stmt ->bind_param("sss",$topicID, $topicArea, $topicName);
-/*
-if($topicArea=="" || $topicName==""){
+$stmt = $conn->prepare("INSERT INTO topic (topic_name, topic_description) VALUES (?,?)");
+$stmt ->bind_param("ss", $topicName, $topicDescription);
+
+if($topicName=="" || $topicDescription==""){
     header("Location: createtopic.php?post=failed");
-}else if(strlen($topicArea)>100){
+}else if(strlen($topicName)>100){
     header("Location: createtopic.php?post=failedtooLong");
 }else if($user_id==""){
     header("Location: createtopic.php?post=failedNoLogin");
 }else{
     $stmt->execute();
-    header("Location: index.php?post=success");
+    header("Location: index.php?subforum=created");
 }
-*/
+
 ?>
