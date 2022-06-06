458c85e8c84d68e61e5e2942f49e100e185b6840
MattieWasTaken <matthewcastles@hotmail.com>
2022-06-06 13:28:59 +1000

    Update index.php

--- index.php
+++ index.php
@@ -342,30 +342,7 @@ include_once('databaseConnection.php');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
-
-  <?php
-if(isset($_REQUEST['createAccount'])){
-  if($_GET['createAccount']== "success"){
-    echo "<script type='text/javascript'>
-    alert('Account Created, Please Sign In To Continue') </script>";
-  }
-}
-  if(isset($_REQUEST['Login'])){
-    if($_GET['Login']== "success"){
-      echo "<script type='text/javascript'>
-      alert('Login Successful') </script>";
-    }
-}
-if(isset($_REQUEST['logout'])){
-  if($_GET['logout']== "SessionTimedOut"){
-    echo "<script type='text/javascript'>
-    alert('Your Session Expired After 30 Minutes of Inactivity') </script>";
-  }
-}
-?>
-
-
-
+  
   <?php include_once('footer.php')?>
 
 
